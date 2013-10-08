52f6a1c6929c60d48371a14e5208dc05bddb01ca
Martin <martin.janocko.gih@gmail.com>
2013-10-08 12:17:57 +0200

    thread 0 pridany

--- test_pakety/src/test_pakety/PacketCapturer.java
+++ test_pakety/src/test_pakety/PacketCapturer.java
@@ -1,21 +1,16 @@
 package test_pakety;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 import org.jnetpcap.Pcap;
-import org.jnetpcap.PcapBpfProgram;
 import org.jnetpcap.PcapIf;
-import org.jnetpcap.nio.JBuffer;
-import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.packet.PcapPacketHandler;
-import org.jnetpcap.protocol.network.Ip4;
  
 public class PacketCapturer {
  
 	public static ArrayList<String> devArray;
+	public static PcapIf device = new PcapIf();
 	
 	public static String byteToHex(byte b) {
 	    int i = b & 0xFF;
@@ -33,7 +28,7 @@ public class PacketCapturer {
 	} 
 
     
-	@SuppressWarnings({ "resource", "unchecked", "rawtypes" })
+	@SuppressWarnings({ "unchecked", "rawtypes", "resource" })
 	public static void main(String[] args) {
 		
 		int i = 0;
@@ -77,123 +72,32 @@ public class PacketCapturer {
                 //Gui.vypis(String.format("#%d: %s [%s]\n", i, device.toString(), description));
             }
             
-            System.out.println("Vyberte 1. zariadenie");
-            int number = new Scanner(System.in).nextInt();
-            PcapIf device =  alldevs_tmp.get(number - 1);
-
-            /*
-            System.out.println("Vyberte 2. zariadenie");
-            number = new Scanner(System.in).nextInt();
-            PcapIf device2 = alldevs_tmp.get(number - 1);
-			*/
+            System.out.println("Vyberte zariadenie: ");
             
-            int snaplen = 64 * 1024;           // cely packet
-            int flags = Pcap.MODE_PROMISCUOUS; // vsetko co pride na sietovu kartu
-            int timeout = 1 * 1000;           // 10ms
- 
-            //Open the selected device to capture packets
-            Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+            Scanner scan_n = new Scanner(System.in);
+            Integer n = scan_n.nextInt();
             
-            if (pcap == null) {
-                System.err.printf("Error while opening device for capture: " + errbuf.toString());
-                return;
-            }
-            
-            System.out.println("Zariadenia otvorene");
- 
-            //set filter
-            System.out.println("Zadajte filter: ");
-            
-            PcapBpfProgram program = new PcapBpfProgram();
-            //String filter = "ip proto \\icmp";
-            String filter = new Scanner(System.in).nextLine();
-            int opt = 0;
-            int mask = 0xffffff00;
-
-            if (pcap.compile(program, filter, opt, mask) != Pcap.OK) {
-            	System.out.println(pcap.getErr());
-            	return;
-            }
-            
-            if (pcap.setFilter(program) != Pcap.OK) {
-            	System.out.println(pcap.getErr());
-            	return;
-            }
-         
-            System.out.println("Zacinam tahat packety ...");
-					
-            PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
-
-            	int n_arp, n_tcp, n_udp, n_icmp;
-				int n_raw, n_snap, n_llc, n_ipx, n_sap;
-				int unkw;
-				int actual;
-				int port;
-		            	
-    			public void nextPacket(PcapPacket packet, String user) {
-
-    				JBuffer buffer = packet;
-    				Ip4 ip = new Ip4();
-    				
-    				actual = buffer.getUShort(12);
-    				
-    				if (actual >= 1536) {
-    					//je to ethernet 2 atd...
-    					if (actual == 2054)
-    						n_arp++;
-    					else if (actual == 2048) {
-	    						switch (buffer.getUByte(23)) {
-    							case 1: n_icmp++; break;
-    							case 6: n_tcp++; break;
-    							case 17: n_udp++; break;
-    							default: unkw++; break;
-    						}
-    					}
-    				}
-    				else if (actual > 0 && actual <= 1500) {
-    					//je to ieee atd...
-    					if (buffer.getUByte(14) == 3)
-    						n_llc++;
-    					else {
-    						switch(buffer.getUShort(14)) {
-    							case 43690: n_snap++; break;
-    							case 57568: n_ipx++; break;
-    							case 61680: n_sap++; break;
-    							case 65535: n_raw++; break;
-    							default: unkw++; break;
-    						}
-    					}
-    				}
-		    				
-    				System.out.printf("%s, ethertype: %d, port: %d, caplen: %d, len: %d, dip: %s, sip: %s\narp: %d, tcp: %d, udp: %d, icmp: %d, raw: %d, snap: %d, llc: %d, ipx: %d, sap: %d, unkw: %d, user: %s\n", 
-    						new Date(packet.getCaptureHeader().timestampInMillis()),
-    						buffer.getUShort(12),
-    						port,
-    						packet.getCaptureHeader().caplen(), 
-    						packet.getCaptureHeader().wirelen(),
-    						org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ip).destination()),
-    						org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ip).source()),
-    						n_arp,
-    						n_tcp,
-    						n_udp, 
-    						n_icmp,
-    						n_raw, 
-    						n_snap, 
-    						n_llc, 
-    						n_ipx, 
-    						n_sap,
-    						unkw,
-    						user
-    				);
-    			}
-            };
-					
-    		pcap.loop(Pcap.LOOP_INFINITE, jpacketHandler, "pcap");
-            
-            pcap.close();
+            device = alldevs_tmp.get(n - 1);
             
         } catch (Exception e) {
             System.out.println(e);
         }
+        
+        //while (true) {
+        	port_0.start();
+        
+        
     }
+	
+	static Thread  port_0 = (new Thread(new Runnable() {
+		
+		PacketHandler ph = new PacketHandler();
+		
+		public void run() {
+			while(true) {
+				ph.getPacket(device, null);
+			}
+		}
+	}));
+	
 }
--- test_pakety/src/test_pakety/PacketHandler.java
+++ test_pakety/src/test_pakety/PacketHandler.java
@@ -1,10 +1,117 @@
 package test_pakety;
 
+import java.util.Date;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapBpfProgram;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.nio.JBuffer;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.PcapPacketHandler;
+import org.jnetpcap.protocol.network.Ip4;
+
 public class PacketHandler {
 	
-	public void packet () {
+	StringBuilder errbuf;
+	int snaplen = 64 * 1024;           	// cely packet
+    int flags = Pcap.MODE_PROMISCUOUS; 	// vsetko co pride na sietovu kartu
+    int timeout = 1 * 1000;           	// 10ms
+	
+	public void getPacket (PcapIf device, String filter) {
 		
+        Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+        
+        if (pcap == null) {
+            System.err.printf("Error while opening device for capture: " + errbuf.toString());
+            return;
+        }
+        
+        System.out.println(device.getName() + " otvorene.");
+
+        //nastavenie filtru
+        PcapBpfProgram program = new PcapBpfProgram();
+        int opt = 0;
+        int mask = 0xffffff00;
+
+        if (pcap.compile(program, filter, opt, mask) != Pcap.OK) {
+        	System.out.println(pcap.getErr());
+        	return;
+        }
+        
+        if (pcap.setFilter(program) != Pcap.OK) {
+        	System.out.println(pcap.getErr());
+        	return;
+        }
+
+        System.out.println("Zacinam tahat packety ...");
 		
+        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
+
+        	int n_arp, n_tcp, n_udp, n_icmp;
+			int n_raw, n_snap, n_llc, n_ipx, n_sap;
+			int unkw;
+			int actual;
+			int port;
+	            	
+			public void nextPacket(PcapPacket packet, String user) {
+
+				JBuffer buffer = packet;
+				Ip4 ip = new Ip4();
+				
+				actual = buffer.getUShort(12);
+				
+				if (actual >= 1536) {
+					//je to ethernet 2 atd...
+					if (actual == 2054)
+						n_arp++;
+					else if (actual == 2048) {
+    						switch (buffer.getUByte(23)) {
+							case 1: n_icmp++; break;
+							case 6: n_tcp++; break;
+							case 17: n_udp++; break;
+							default: unkw++; break;
+						}
+					}
+				}
+				else if (actual > 0 && actual <= 1500) {
+					//je to ieee atd...
+					if (buffer.getUByte(14) == 3)
+						n_llc++;
+					else {
+						switch(buffer.getUShort(14)) {
+							case 43690: n_snap++; break;
+							case 57568: n_ipx++; break;
+							case 61680: n_sap++; break;
+							case 65535: n_raw++; break;
+							default: unkw++; break;
+						}
+					}
+				}
+	    				
+				System.out.printf("%s, ethertype: %d, port: %d, caplen: %d, len: %d, dip: %s, sip: %s\narp: %d, tcp: %d, udp: %d, icmp: %d, raw: %d, snap: %d, llc: %d, ipx: %d, sap: %d, unkw: %d, user: %s\n", 
+						new Date(packet.getCaptureHeader().timestampInMillis()),
+						buffer.getUShort(12),
+						port,
+						packet.getCaptureHeader().caplen(), 
+						packet.getCaptureHeader().wirelen(),
+						org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ip).destination()),
+						org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ip).source()),
+						n_arp,
+						n_tcp,
+						n_udp, 
+						n_icmp,
+						n_raw, 
+						n_snap, 
+						n_llc, 
+						n_ipx, 
+						n_sap,
+						unkw,
+						user
+				);
+			}
+        };
+        
+        pcap.loop(1, jpacketHandler, "pcap");
+        pcap.close();
 	}
 
 }
